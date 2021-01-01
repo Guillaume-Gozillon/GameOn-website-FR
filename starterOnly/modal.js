6bd1bed803794bb9942c00ea6e9ec2983c0d9333
Gozillo <guillaumegozillon@gmail.com>
2021-01-01 02:18:07 +0100

    Launch modalForm + HTML Indentation

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,13 +11,17 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+//Creat new const to close the modalBg
+const spanX = document.getElementById("close-modale")
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
-
-
+// Close modal
+spanX.addEventListener("click", function() {
+  modalbg.style.display = "none"
+})
+// END modal
